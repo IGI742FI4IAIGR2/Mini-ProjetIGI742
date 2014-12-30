dd9d68b7e53ed0f0c5e978e47f9380a1f6438ea6
Mosquito74 <nathandu01@gmail.com>
2014-12-30 14:22:58 +0100

    Génération des Getters et des Setters pour la classe Abonnement

--- mediatheque/src/Abonnement.java
+++ mediatheque/src/Abonnement.java
@@ -32,5 +32,39 @@ public class Abonnement {
 	 */
 	public TypeAbonnement typeAbonnement;
 
+	// Getters & Setters
+	
+	public int getIdAbonnement() {
+		return idAbonnement;
+	}
+
+	public void setIdAbonnement(int idAbonnement) {
+		this.idAbonnement = idAbonnement;
+	}
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public DureeAbonnement getDureeAbonnement() {
+		return dureeAbonnement;
+	}
+
+	public void setDureeAbonnement(DureeAbonnement dureeAbonnement) {
+		this.dureeAbonnement = dureeAbonnement;
+	}
+
+	public TypeAbonnement getTypeAbonnement() {
+		return typeAbonnement;
+	}
+
+	public void setTypeAbonnement(TypeAbonnement typeAbonnement) {
+		this.typeAbonnement = typeAbonnement;
+	}
+
 
 }
