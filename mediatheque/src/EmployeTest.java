d7d553c3e89e0faddd873a5854a09baf867aae10
NATHAN <NATHAN@10.7.235.45>
2015-01-07 12:43:19 +0100

    Developpement Classe Test EmployeTest.java

--- mediatheque/src/EmployeTest.java
+++ mediatheque/src/EmployeTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 /**
@@ -8,6 +10,7 @@ public class EmployeTest extends TestCase {
 
 	
 	private Employe employe = new Employe(1);
+	protected static Fabrique fabrique;
 	
 	public void testEmploye() {
 		fail("Not yet implemented");
@@ -15,8 +18,7 @@ public class EmployeTest extends TestCase {
 	
 	/**
 	 * test de la modification de l'état d'un exemplaire
-	 * @author mikael
-	 * @param Produit
+	 * @author nathan
 	 */
 	public void testModifierEtatExemplaire() {
 		Produit p = new Produit();
@@ -27,35 +29,91 @@ public class EmployeTest extends TestCase {
 			fail("Erreur");
 		}
 	}
-
+	
+	/**
+	 * La methode commanderProduit n'a pas ete developpe dans la classe Employe
+	 */
 	public void testCommanderProduit() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * test de la methode renouvellerAbonnement
+	 * @author nathan
+	 */
 	public void testRenouvellerAbonnement() {
+		Membre m = new Membre();
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * test de la methode supprimerAbonnement
+	 * @author nathan
+	 */
 	public void testSupprimerAbonnement() {
-		fail("Not yet implemented");
+		Membre m = new Membre();
+		Abonnement a = new Abonnement();
+		m.setMonAbonnement(a);
+		m.getMonAbonnement().setDureeAbonnement(DureeAbonnement.troisMois);
+		employe.supprimerAbonnement(m);
+		if(!m.getMonAbonnement().getDureeAbonnement().equals(DureeAbonnement.expire)){
+			fail("erreur");
+		}
 	}
-
+	
+	/**
+	 * La methode editerInfoMembre n'a pas ete developpe dans la classe Employe
+	 */
 	public void testEditerInfoMembre() {
 		fail("Not yet implemented");
 	}
-
+	
+	/**
+	 * test de la methode supprimerMembre
+	 * cas d'un membre et d'un employe d'une mediatheque differente non traite
+	 * @author nathan
+	 */
 	public void testSupprimerMembre() {
-		fail("Not yet implemented");
+		Membre m = new Membre();
+		Abonnement a = new Abonnement();
+		a.setTypeAbonnement(TypeAbonnement.LivDVDSimple);
+		Mediatheque media = new Mediatheque("Novel","3 rue de Novel 74000 Annecy",fabrique);
+		employe.setMaMediateque(media);
+		m.setMaMediateque(media);
+		m.setMonAbonnement(a);
+		employe.supprimerMembre(m);
+		if (!m.getMonAbonnement().getTypeAbonnement().equals(TypeAbonnement.PlusAbonne)){
+			fail("Erreur");
+		}
 	}
 
+	/**
+	 * test de la methode ajouterMembre
+	 * @author nathan
+	 */
+	//encore un pb
 	public void testAjouterMembre() {
-		fail("Not yet implemented");
+		Membre m = new Membre();
+		ArrayList<Membre> listeMembre = new ArrayList<Membre>();
+		Mediatheque media = new Mediatheque("Novel","3 rue de Novel 74000 Annecy",fabrique);
+		employe.setMaMediateque(media);
+		media.setListeMembre(listeMembre );
+		employe.ajouterMembre(m);
+		if(!media.getListeMembre().contains(m)){
+			fail("Erreur");
+		}
 	}
-
+	
+	/**
+	 * La methode gererCarte n'a pas ete developpe dans la classe Employe
+	 */
 	public void testGererPerteCarte() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * La methode attribuerCarte n'a pas ete developpe dans la classe Employe
+	 */
 	public void testAttribuerCarte() {
 		fail("Not yet implemented");
 	}
