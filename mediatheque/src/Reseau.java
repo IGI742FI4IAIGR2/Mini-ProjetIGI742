f6b7e9fe1bab4d325aabfccd83b74e05d75e28a6
mikael10j <mikael.jacquemont@free.fr>
2015-01-07 15:04:19 +0100

    modif de reseau

--- mediatheque/src/Reseau.java
+++ mediatheque/src/Reseau.java
@@ -6,11 +6,11 @@ import java.util.*;
  */
 public class Reseau {
 
-	protected static Fabrique fabrique;
+	
 	
 	public static void main(String[] args){
-		
-		Mediatheque media1 = new Mediatheque("Novel","3 rue de Novel 74000 Annecy",fabrique);
+		Fabrique f1 = new Fabrique();
+		Mediatheque media1 = new Mediatheque("Novel","3 rue de Novel 74000 Annecy",f1);
 	}
 
 }
