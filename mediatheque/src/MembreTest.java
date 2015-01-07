7e567e36e1ba2dc04101f346198329cd8e05d792
ma2s74 <ma_2_s@hotmail.fr>
2015-01-07 10:03:12 +0100

    modif des classes test et membreTest

---
+++ mediatheque/src/MembreTest.java
@@ -0,0 +1,24 @@
+import junit.framework.TestCase;
+
+
+public class MembreTest extends TestCase {
+
+	protected void setUp() throws Exception {
+	}
+
+	public void testIdentification() {
+		// Création du membre
+		Membre m1=new Membre();
+		m1.identification("jhvbn","juvkjh");
+			
+	}
+
+	public void testDemandeEmprunt() {
+		fail("Not yet implemented");
+	}
+
+	public void testVerifierDisponibilite() {
+		fail("Not yet implemented");
+	}
+
+}
