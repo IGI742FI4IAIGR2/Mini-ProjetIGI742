2f478d9946bd1a2360d4a25d5eb30ca3af5b16a8
ma2s74 <ma_2_s@hotmail.fr>
2015-01-07 12:05:11 +0100

    testmembremodif

--- mediatheque/src/MembreTest.java
+++ mediatheque/src/MembreTest.java
@@ -5,19 +5,31 @@ public class MembreTest extends TestCase {
 
 	protected void setUp() throws Exception {
 	}
-	
+	private Membre m1 = new Membre();
+	private Mediatheque Centre = new Mediatheque("MediaCentre", "chemin la rose", Fabrique fabriqueC);
 	public void testIdentification() {
-		// Création du membre
-		Membre m1 = new Membre();
+		// Création du membre		
 		m1.setIdMembre("id74");
-		m1.getmdp("1234");		
+		m1.setmdp();		
 		m1.identification("id74","1243");
-		if (m1.identification("id74","1243")){
+		if (m1.identification("id74","1243")) {
 			fail("Erreur");
+		}
 	}
 
 	public void testDemandeEmprunt() {
-		fail("Not yet implemented");
+		Produit p = new Produit();
+		new Exemplaire(p);
+		demandeEmprunt(p);
+		if (p.DemandeEmprunt("id74","1243")) {
+			fail("ok ");
+		}
+		
+	}
+
+	private void demandeEmprunt(Produit p) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public void testVerifierDisponibilite() {
