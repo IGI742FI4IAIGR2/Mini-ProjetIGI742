3c5f1aa26a7d9c22c567e349a80f8ccc66d8a6bb
ma2s74 <ma_2_s@hotmail.fr>
2015-01-08 15:47:29 +0100

    rien de spécial

--- mediatheque/src/MembreTest.java
+++ mediatheque/src/MembreTest.java
@@ -1,37 +1,31 @@
-import junit.framework.TestCase;
 
+import junit.framework.TestCase;
+import java.util.ArrayList;
 
 public class MembreTest extends TestCase {
 
 	protected void setUp() throws Exception {
 	}
-	private Membre m1 = new Membre();
-	private Mediatheque Centre = new Mediatheque("MediaCentre", "chemin la rose", Fabrique fabriqueC);
-	public void testIdentification() {
-		// Création du membre		
+	protected static Fabrique fabrique;
+	private Membre m1 = new Membre();// Création du membre	
+	private Produit p = new Produit();// Création d'un produit 
+	private Exemplaire exP = new Exemplaire(p);// Création d'un exemplaire de ce produit	
+	private Mediatheque Centre = new Mediatheque("Novel","3 rue de Novel 74000 Annecy",fabrique);
+	
+	public void testIdentification() {	
 		m1.setIdMembre("id74");
 		m1.setmdp();		
 		m1.identification("id74","1243");
 		if (m1.identification("id74","1243")) {
-			fail("Erreur");
+			fail("ok");
 		}
 	}
 
-	public void testDemandeEmprunt() {
-		Produit p = new Produit();
-		new Exemplaire(p);
-		demandeEmprunt(p);
-		if (p.DemandeEmprunt("id74","1243")) {
+	/*public void testDemandeEmprunt() {
+		m1.setDateEmprunt(2014,05,12);
+		p.demandeEmprunt(exP);
+		if (p.DemandeEmprunt()) {
 			fail("ok ");
-		}
-		
-	}
-
-	private void demandeEmprunt(Produit p) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void testVerifierDisponibilite() {
-		fail("Not yet implemented");
-	}
+		}		
+	}*/
 }
